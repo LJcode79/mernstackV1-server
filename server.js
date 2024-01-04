90f7f7f3b73e28df0bb3189485dc40b18c2df4c7
Lawrence John Balanza <73139121+LJcode79@users.noreply.github.com>
2024-01-03 23:45:30 -0800

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors({
-    origin: ['http://localhost:3000/', 'https://ljbuddy.netlify.app/','*'],
+    origin: ['http://localhost:3000/', 'https://ljbuddy.netlify.app','*'],
     credentials: true
   }))
 
