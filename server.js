b4bc27419974153095358867e8efe08b4000b6dc
Lawrence John Balanza <73139121+LJcode79@users.noreply.github.com>
2025-04-29 21:35:13 -0700

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const app = express()
 app.use(cors({
     origin: ['http://localhost:3000/', 'https://ljbuddy.netlify.app','*'],
     credentials: true
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
   }))
 
 
